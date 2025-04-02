af877e2974b8e52e6fe5571f44507dd42f6393e3
baibhabkarmakar <baibhabkarmakar29@gmail.com>
2025-04-02 10:15:33 +0530

    Day 2 completed

--- Day_1/FirstClass.java
+++ Day_1/FirstClass.java
@@ -1,7 +1,4 @@
-package Day_1;
-
 class FirstClass {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
     } 
 }
---
+++ Day_2/Code.java
@@ -0,0 +1,62 @@
+public class Code {
+    public static void main(String args[]) {
+        // output : 
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World \nBaibhab Karmakar");
+
+
+        // Question : Print a Pattern : 
+        // *
+        // * *
+        // * * *
+        // * * * *
+
+        // System.out.println("*");
+        // System.out.println("* *");
+        // System.out.println("* * *");
+        // System.out.println("* * * *");
+
+        // variables : 
+
+        // String name = "Tony Stark";
+        // int a = 25 , b = 10;
+        // int age = 20;
+        // double price = 25.25;
+
+        // b = 20;
+        // name = "Saktiman";
+
+
+        // int a = 10;
+        // int b = 25;
+
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        // int diff = b - a;
+        // System.out.println(diff);
+
+        // int mul = a * b;
+        // System.out.println(mul);
+
+        // float div = b/a;
+        // System.out.println(div);
+
+
+        // Inputs : 
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("The sum of the 2 inputed numbers are : " + sum);
+
+        // String name = sc.next();    // this will take only one word as input .
+        // System.out.println("Your name is : " + name);
+
+        // String fullName = sc.nextLine();    // this will take the full name as input . 
+        // System.out.println("Your full name is : " + fullName);
+        // sc.close();
+
+    }
+}
