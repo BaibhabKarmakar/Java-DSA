9601f18dc5d79b83481920803c48254deaf32660
Baibhab Karmakar <baibhabkarmakar29@gmail.com>
2025-04-06 20:02:20 +0530

    day 4 completed

---
+++ Day_4/Code.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Code {
+    public static void main(String args[]) {
+        // For loop : 
+ 
+        // Question : print "Hello World" till the counter reaches the breaaking point .
+        // for(int counter = 0 ; counter < 100 ; counter++) {
+        //     System.out.println("Hello World!" + (counter + 1));
+        // }
+
+        // Question : print the numbers from 1 to n through a 'for' loop : 
+
+        // for(int i = 0 ; i < 10 ; i++) {
+        //     System.out.print(i+1 + " ");
+        // }
+
+        // Question : print the numbers from 1 to n through a 'while' loop : 
+        
+        // int i = 12;
+        // while( i < 10) {
+        //     System.out.print(i+1 + " ");
+        //     i++;
+        // }
+
+        // Question : print the numbers from 1 to n through a "do - while" loop :
+
+        // int i = 12;
+        // do {
+        //     System.out.print(i+1 + " ");
+        //     i++;
+        // }
+        // while(i < 10);
+
+
+        // Question : Print the first N natural numbers : 
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for(int i = 1 ; i <= n ; i++) {
+        //     sum += i;
+        // }
+
+        // System.out.println("Tthe sum is : " + sum);
+
+        // Question : Print the table of a number input by the user : 
+
+        // int number = sc.nextInt();
+        // for(int i = 1 ; i <= number ; i++) {
+        //     System.out.println(number + " X " + i + " = " + number * i);
+        // }
+        sc.close();
+    }
+}
